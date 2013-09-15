a2d5a2ac98a82660209bbf5851497a30d29236bb
Nikolay Nemshilov <nemshilov@gmail.com>
2013-09-15 16:58:39 +1000

    Adding simple dimensions (sizes/positions) handling

--- lib/under_os/point.rb
+++ lib/under_os/point.rb
@@ -2,6 +2,28 @@
 # Generic Point/Size unit
 #
 class Point
+
+  def initialize(x, y=nil)
+    if x.is_a?(Point)
+      y = x.y if x.y
+      x = x.x
+    elsif x.is_a?(Hash)
+      y = x[:y] || x['y'] || nil
+      x = x[:x] || x['x'] || nil
+    end
+
+    @x = x
+    @y = y
+  end
+
+  def x
+    @x
+  end
+
+  def y
+    @y
+  end
+
   def ==(point)
     x == point.x && y == point.y
   end
--- lib/under_os/ui/dimensions.rb
+++ lib/under_os/ui/dimensions.rb
@@ -2,13 +2,35 @@
 # This module handles generic ui dimensions and positions of views
 #
 module UnderOs::UI::Dimensions
-  def size
-    @size ||= UnderOs::UI::Dimensions::Size.new(self)
+  def size(size=nil)
+    if size
+      self.size = size
+      self
+    else
+      @size ||= UnderOs::UI::Dimensions::Size.new(self)
+    end
   end
 
   def size=(size)
-    self.size.x = size.x
-    self.size.y = size.y
+    size = Point.new(size) # cleaning up
+
+    self.size.x = size.x if size.x
+    self.size.y = size.y if size.y
+  end
+
+  def position(position=nil)
+    if position
+      self.position = position
+    else
+      @position ||= UnderOs::UI::Dimensions::Position.new(self)
+    end
+  end
+
+  def position=(position)
+    position = Point.new(position) # cleaning up
+
+    self.position.x = position.x if position.x
+    self.position.y = position.y if position.y
   end
 
   class Size < UnderOs::Point
@@ -30,6 +52,28 @@ module UnderOs::UI::Dimensions
 
     def y=(size)
       @view.style.height = size
+    end
+  end
+
+  class Position < UnderOs::Point
+    def initialize(view)
+      @view = view
+    end
+
+    def x
+      @view.style.left
+    end
+
+    def x=(position)
+      @view.style.left = position
+    end
+
+    def y
+      @view.style.top
+    end
+
+    def y=(position)
+      @view.style.top = position
     end
   end
 end
