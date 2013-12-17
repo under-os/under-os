93cad6bd4cf45b3e41b27b301160215565333bcc
Peter Hellberg <peter@c7.se>
2013-12-17 01:47:08 +0100

    Added missing HTTP class methods on UnderOs::HTTP
     - Also added a spec using Facon

--- lib/under_os/http.rb
+++ lib/under_os/http.rb
@@ -2,4 +2,24 @@ class UnderOs::HTTP
   def self.get(url, options={}, &block)
     Request.new(url, options.merge(method: :get), &block).send
   end
+
+  def self.post(url, options={}, &block)
+    Request.new(url, options.merge(method: :post), &block).send
+  end
+
+  def self.put(url, options={}, &block)
+    Request.new(url, options.merge(method: :put), &block).send
+  end
+
+  def self.delete(url, options={}, &block)
+    Request.new(url, options.merge(method: :delete), &block).send
+  end
+
+  def self.patch(url, options={}, &block)
+    Request.new(url, options.merge(method: :patch), &block).send
+  end
+
+  def self.head(url, options={}, &block)
+    Request.new(url, options.merge(method: :head), &block).send
+  end
 end
