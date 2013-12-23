339ba5293d29119acf28effef2d677ff7a215f38
Nikolay Nemshilov <nemshilov@gmail.com>
2013-12-24 10:04:26 +1100

    A bit more straight forward rubymotion initialization hooks

--- lib/under-os.rb
+++ lib/under-os.rb
@@ -8,16 +8,14 @@ Motion::Project::App.instance_eval do
       Dir.glob(File.dirname(__FILE__) + '/**/*.rb').reverse.each do |file|
         app.files.insert(0, file) if file != __FILE__
       end
-    end
 
-    setup_before_under_os *args do |app|
       app.resources_dirs << File.dirname(__FILE__) + "/assets"
       app.resources_dirs << "app/styles/"  if File.exists?("app/styles")
       app.resources_dirs << "app/layouts/" if File.exists?("app/layouts")
 
       app.fonts << "fontawesome-webfont.ttf"
-
-      instance_exec app, &block
     end
+
+    setup_before_under_os *args, &block
   end
 end
